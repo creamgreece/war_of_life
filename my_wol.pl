09779cde43de3e326f1ed6fbe5e5928aa28dcacf
Xueqi Chen <xc1611@ic.ac.uk>
2013-02-27 18:05:46 +0000

    ,,,

--- my_wol.pl
+++ my_wol.pl
@@ -33,28 +33,64 @@ smallest(A,B,A):-
 smallest(A,B,B):-
   A >= B.
 
+
+bloodiest_move([],_,Move,Least).
+bloodiest_move([[A,B,MA,MB]|PossMoves],[AliveBlues,AliveReds],Move,Least) :-
+   alter_board([A,B,MA,MB],AliveReds,NextReds),
+   next_generation([AliveBlues,NextReds],[NextBlues,_]),
+    length(NextBlues,L),
+    ( Least > L
+    -> bloodiest_move(PossMoves,[AliveBlues,AliveReds],[A,B,MA,MB],L);
+       bloodiest_move(PossMoves,[AliveBlues,AliveReds],Move,Least)).
+
 %%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%% BLOODLUST STRATEGY
 bloodlust('r', [AliveBlues, AliveReds],  [NewAliveBlues, NewAliveReds], Move):-
- findall([A,B,MA,MB],(member([A,B], [AliveBlues,AliveReds]),
+ findall([A,B,MA,MB],(member([A,B], AliveReds),
                       neighbour_position(A,B,[MA,MB]),
                       \+member([MA,MB],AliveReds),
 	                    \+member([MA,MB],AliveBlues)),PossMoves),
      setof((NewAliveBlues,[MA,MB]),alter_board([MA,MB],AliveBlues,NewAliveBlues),[(NewAliveBlues,Move)|_]).
 bloodlust('b', [AliveBlues, AliveReds],  [NewAliveBlues, NewAliveReds], Move):-
- findall([A,B,MA,MB],(member([A,B], [AliveBlues,AliveReds]),
+ findall([A,B,MA,MB],(member([A,B], AliveBlues),
                       neighbour_position(A,B,[MA,MB]),
                       \+member([MA,MB],AliveReds),
 	                    \+member([MA,MB],AliveBlues)),PossMoves),
      setof((NewAliveReds,[MA,MB]),alter_board([MA,MB],AliveReds,NewAliveReds),[(NewAliveReds,Move)|_]).
-/*
+/*    
+   
 
- bloodiest_move(0,PossMoves,SmallestRed,Move) 
+testConfig([[2,2],[2,3],[2,4],[3,3]],[[4,4],[8,8],[7,8],[7,7],[8,7]]).
+
+test(Board):-
+testConfig(Board),
+drawBoard(Board),
+
+
+findall([A,B,MA,MB],(member([A,B], [[4,4],[8,8],[7,8],[7,7],[8,7]]),neighbour_position(A,B,[MA,MB]),\+member([MA,MB],[[4,4],[8,8],[7,8],[7,7],[8,7]]),\+member([MA,MB],[[2,2],[2,3],[2,4],[3,3]])),PossMoves),bloodiest_move(PossMoves,[[[2,2],[2,3],[2,4],[3,3]],[[4,4],[8,8],[7,8],[7,7],[8,7]]],Move,65). 
+
+
+
+findall((L,[MA,MB]),(alter_board([A,B,MA,MB],[[4,4],[8,8],[7,8],[7,7],[8,7]],NewReds), next_generation([[[2,2],[2,3],[2,4],[3,3]],NewReds],[NextBlues,_]), length(NewBlues,L), member([A,B,MA,MB],PossMoves)), Ans). 
+
+
+
+[[4,4,3,4],[4,4,3,5],[4,4,4,3],[4,4,4,5],[4,4,5,3],[4,4,5,4],[4,4,5,5],[7,8,6,7],[7,8,6,8],[7,7,6,6],[7,7,6,7],[7,7,6,8],[7,7,7,6],[7,7,8,6],[8,7,7,6],[8,7,8,6]]
+
+
+next_generation([[[2,2],[2,3],[2,4],[3,3]],NewReds],NewBoard).
+
+alter_board([A,B,MA,MB], Alives, NewAlives) :-
+
  alterBoard(Move, AliveReds, NewAliveReds),
  smallest(LeastReds,AliveReds, LeastReds),
   Move
   Least Reds
+
+
+alter_board([4,4,4,3],[[1,8],[4,4],[8,8]],NewReds), next_generation([[[2,2],[2,3],[2,4],[3,3]],NewReds],NewBoard),write(NewBoard).
+
 
 random_move(Alive, OtherPlayerAlive, Move) :-
  findall([A,B,MA,MB],(member([A,B], Alive),
